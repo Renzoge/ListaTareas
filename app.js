3c65a9d28711a998c9b8e6203d3b1a765b6e922e
Renzo Reyes <renzoestebanreyes@gmail.com>
2025-08-24 16:40:41 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,49 @@
+//Selección
+const campo = document.getElementById("campo");
+
+const agregar = document.getElementById("agregar");
+
+const tareas = document.getElementById("lista-tareas");
+
+
+
+
+
+//Agregar tarea
+function insertarTarea() {
+    //Creación de los elementos
+
+const nuevoTexto = document.createElement("span");
+nuevoTexto.classList.add("texto");
+
+const nuevoBoton = document.createElement("button");
+ nuevoBoton.textContent = "X";
+ nuevoBoton.classList.add("eliminar");
+
+const tareaNueva = document.createElement("li");
+        //Anidando elementos...
+tareaNueva.append(nuevoTexto, nuevoBoton);
+tareaNueva.classList.add("lis");
+    
+            //Lógica
+
+    if (campo.value === "") {
+        campo.classList.add("error")
+    } else {
+        nuevoTexto.innerText = campo.value;
+        campo.classList.remove("error")
+        tareas.append(tareaNueva);   
+    }
+            //Eliminar tarea
+
+         nuevoBoton.onclick= function(){
+            tareaNueva.remove();
+         }   
+    
+}
+
+agregar.onclick = insertarTarea;
+
+
+
+ 
